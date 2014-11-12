5359bf24b9f93378a1c52ba92bf5326d8928989b
rogersb11 <brettrogers11@gmail.com>
2014-11-11 23:30:40 -0500

    Oops, fix deleting needed sensor

--- t0lte/unpack-boot.img/boot.img-ramdisk/sbin/ext/sensors.sh
+++ t0lte/unpack-boot.img/boot.img-ramdisk/sbin/ext/sensors.sh
@@ -32,7 +32,6 @@ cd /;
 # copy new sensors
 rm -rf /system/bin/sensorservice;
 rm -rf /system/lib/hw/sensors.smdk4x12.so;
-rm -rf /system/lib/libsensorservice.so;
 cp -a /res/misc/sensors/sensorhubservice /system/bin/sensorhubservice;
 cp -a /res/misc/sensors/libsensorhubservice.so /system/lib/libsensorhubservice.so;
 cp -a /res/misc/sensors/sensorhubs.smdk4x12.so /system/lib/hw/sensorhubs.smdk4x12.so;
